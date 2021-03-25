456b9d6453bc73f2b7b080495f6992f211307f6b
sam921611 <sam921611@kimo.com>
2021-03-25 13:10:48 +0800

    Initial commit

---
+++ vue_firestore_employeemanager-master/src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+	apiKey: "AIzaSyDxIld6mN-AuzPMl_ktgKgtiGlSxarBF00",
+	authDomain: "linkchattest.firebaseapp.com",
+	databaseURL: "https://linkchattest.firebaseio.com",
+	projectId: "linkchattest",
+	storageBucket: "linkchattest.appspot.com",
+	messagingSenderId: "798585863063",
+}
